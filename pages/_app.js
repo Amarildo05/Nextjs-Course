f320a927441f855cbe7c026bad21c4adffeb5c92
Amarildo <amarildokola36@gmail.com>
2023-05-15 22:48:08 +0200

    nextjs-course

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import Layout from '../components/layout/layout'
+import '../styles/globals.css'
+import Head from 'next/head';
+import { NotificationContextProvider } from '../store/notification-context';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
+  ) 
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import EventList from "../components/events/event-list";
+import NewsletterRegistration from "../components/input/newsletter-registration";
+import { getFeaturedEvents } from "../helpers/api-util";
+
+function HomePage(props) {
+    return (
+        <div>
+          <Head>
+            <title>NextJS Events</title>
+            <meta name='description'
+            content='Find the best events that allow you to evolve...'
+            />
+          </Head>
+          <NewsletterRegistration />
+          <EventList items={props.events} />
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents
+    },
+    revalidate: 1800
+  }
+}
+
+export default HomePage;
